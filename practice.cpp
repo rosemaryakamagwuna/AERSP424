1aab8f06a9f2b227f67c392fe1cf5ccb67d149ed
Rosemary Akamagwuna <rosemaryakamagwuna@Rosemarys-MacBook-Pro.local>
2023-08-31 15:59:18 -0400

    first commit

---
+++ practice.cpp
@@ -0,0 +1,11 @@
+int main () {
+    //this is a comment (This is a function that takes in executable code)
+    /*to add comments that span more than one line use this method*/
+
+}
+
+//to say hello world
+#include <iostream>
+int main(){
+    std::cout <<"Hello World."; //iostream allows us
+}
