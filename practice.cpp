35880d7ef7dde610521177033a7f19d8338afd7e
Rosemary Akamagwuna <rosemaryakamagwuna@Rosemarys-MacBook-Pro.local>
2023-08-31 16:53:40 -0400

    second commit

--- practice.cpp
+++ practice.cpp
@@ -1,11 +1,21 @@
-int main () {
+/*int main () {
     //this is a comment (This is a function that takes in executable code)
-    /*to add comments that span more than one line use this method*/
-
-}
+    /*to add comments that span more than one line use this method
+}*/
 
 //to say hello world
-#include <iostream>
+/*#include <iostream>
 int main(){
     std::cout <<"Hello World."; //iostream allows us
+}*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+    
+    std::cout<< "name :rosemary" ;
+    std::cout<<"surname : akamagwuna";
+    return 0;
 }
+
